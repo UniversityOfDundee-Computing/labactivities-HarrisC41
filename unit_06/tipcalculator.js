814feffc1c1ba40214d1b81ffedd7e904abed877
HarrisC41 <harrischoha41@outlook.com>
2022-11-11 12:43:28 +0000

    working in js

---
+++ unit_06/tipcalculator.js
@@ -0,0 +1,12 @@
+document.getElementById("billamount").addEventListener("type", billamount);
+document.getElementById("5").addEventListener("click", 5);
+document.getElementById("10").addEventListener("click", 10);
+document.getElementById("25").addEventListener("click", 25);
+document.getElementById("2+10").addEventListener("click", 2+10);
+
+
+
+
+function billamount() {
+    document.getElementById('tip').value = 'billamount';
+  }
